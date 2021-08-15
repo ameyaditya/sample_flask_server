b467fc0c0c14b163caa9b913d794abc82d5ea3b7
ameyaditya <ameyaditya.j@gmail.com>
2021-08-15 22:30:00 +0530

    first commit

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ from waitress import serve
 
 app = Flask(__name__)
 
-@app.route("/api")
+@app.route("/")
 def home():
     return "Hello World"
 
-@app.route("/api/json")
+@app.route("/json")
 def json():
     return jsonify({"sampleKey": "SampleValue"})
 
-@app.route("/api/dynamic/<did>")
+@app.route("/dynamic/<did>")
 def dynamic(did):
     return jsonify({"key": f"value: {did}"})
 
-@app.route("/api/post_url", methods=['POST'])
+@app.route("/post_url", methods=['POST'])
 def post_url():
     data = request.get_json()
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(port=4000)
-    # serve(app, host='0.0.0.0', port=80)
+    # app.run(port=4000)
+    serve(app, host='127.0.0.1', port=4000)
